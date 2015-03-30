47bacdc0cca6a472311a38085384ce41f9d8b95d
orangeman <flo@sonnenstreifen.de>
2015-03-30 16:05:05 +0200

    transaction view salat

--- app/src/main/java/org/baobab/pos/TransactionFragment.java
+++ app/src/main/java/org/baobab/pos/TransactionFragment.java
@@ -1,32 +1,47 @@
 package org.baobab.pos;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.GridLayout;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.inputmethod.InputMethodManager;
 
 public class TransactionFragment extends Fragment
         implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private float sum;
     private TransactionView transaction;
 
     @Override
     public View onCreateView(LayoutInflater flate, ViewGroup p, Bundle state) {
         View frame = flate.inflate(R.layout.fragment_transaction, null, false);
         transaction = (TransactionView) frame.findViewById(R.id.transaction);
+        transaction.setOnAmountClick(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                getFragmentManager().beginTransaction()
+                        .replace(v.getId(), new NumberDialogFragment(
+                                "Wie viel?", (String) v.getTag()) {
+                            @Override
+                            public void onNumber(float number) {
+                                ContentValues cv = new ContentValues();
+                                cv.put("quantity", number);
+                                getActivity().getContentResolver().update(
+                                        getActivity().getIntent().getData().buildUpon()
+                                            .appendEncodedPath("products/" + v.getId())
+                                            .build(), cv, null, null);
+                            }
+                        }, "amount")
+                        .addToBackStack("amount").commitAllowingStateLoss();
+            }
+        });
         return frame;
     }
 
@@ -43,6 +58,17 @@ public class TransactionFragment extends Fragment
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                    getFragmentManager().popBackStack();
+                }
+            });
+            ((InputMethodManager) getActivity()
+                    .getSystemService(Context.INPUT_METHOD_SERVICE))
+                    .hideSoftInputFromWindow(getView().getWindowToken(), 0);
+        }
         transaction.populate(data);
     }
 
--- app/src/main/java/org/baobab/pos/TransactionView.java
+++ app/src/main/java/org/baobab/pos/TransactionView.java
@@ -4,19 +4,20 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.GridLayout;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class TransactionView extends ScrollView {
+public class TransactionView extends GridLayout {
 
-    private GridLayout products;
-    private float sum;
+    OnClickListener onAmountClick;
+    int sum;
 
     public TransactionView(Context context) {
         this(context, null);
@@ -24,12 +25,16 @@ public class TransactionView extends ScrollView {
 
     public TransactionView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        View.inflate(getContext(), R.layout.view_transaction, this);
-        products = (GridLayout) findViewById(R.id.transaction_products);
+        setColumnCount(6);
+        setRowCount(2);
+    }
+
+    public void setOnAmountClick(OnClickListener onAmountClick) {
+        this.onAmountClick = onAmountClick;
     }
 
     public void populate(Cursor data) {
-        products.removeAllViews();
+        removeAllViews();
         data.moveToPosition(-1);
         sum = 0;
         while (data.moveToNext()) {
@@ -53,26 +58,27 @@ public class TransactionView extends ScrollView {
                 image.setImageURI(img);
                 image.setPadding(2, 2, 2, 2);
                 image.setScaleType(ImageView.ScaleType.FIT_XY);
-                double factor = (2.0 - (1.0/quantity)) / quantity;
-//                double factor = (((double) quantity) - (1 - (1.0/Math.pow(2, quantity-1)))) / quantity;
+                int width = getContext().getResources().getDimensionPixelSize(R.dimen.img_width);
+                int height = getContext().getResources().getDimensionPixelSize(R.dimen.img_height);
+                double factor = (2.0 - (1.0 / quantity)) / quantity;
                 images.addView(image,
-                        new LinearLayout.LayoutParams((int) (67 * factor ), 42));
+                        new LinearLayout.LayoutParams((int) (width * factor ), height));
             }
             images.setClickable(true);
             final long product_id = data.getLong(3);
             images.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    getContext().getContentResolver().delete(
-//                            getContext().getIntent().getData().buildUpon()
-//                                    .appendEncodedPath("products/" + product_id)
-//                                    .build(), null, null);
+                    getContext().getContentResolver().delete(
+                            ((FragmentActivity) getContext()).getIntent().getData().buildUpon()
+                                    .appendEncodedPath("products/" + product_id)
+                                    .build(), null, null);
                 }
             });
             GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
             lp.rowSpec = GridLayout.spec(0, 2);
             lp.topMargin = 8;
-            products.addView(images, lp);
+            addView(images, lp);
         }
         TextView amount = new TextView(getContext());
         amount.setTextSize(getContext().getResources().getDimension(R.dimen.font_size_xlarge));
@@ -81,54 +87,72 @@ public class TransactionView extends ScrollView {
         amount.setText(String.valueOf(quantity));
         GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
         lp.rowSpec = GridLayout.spec(0, 2);
-        products.addView(amount, lp);
+        lp.setGravity(Gravity.RIGHT);
+        amount.setBackgroundResource(R.drawable.background_translucent);
+        amount.setTextColor(getResources().getColor(R.color.xlight_blue));
+        FrameLayout f = new FrameLayout(getContext());
+        f.addView(amount);
+        addView(f, lp);
+        f.setClickable(true);
+        f.setId(data.getInt(3));
+        f.setTag(String.valueOf(quantity));
+        f.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAmountClick.onClick(v);
+            }
+        });
 
         TextView x = new TextView(getContext());
         x.setTextSize(getContext().getResources().getDimension(R.dimen.font_size_small));
-        x.setTextSize(15);
         x.setText("x ");
+        x.setTextColor(getResources().getColor(R.color.light_blue));
         lp = new GridLayout.LayoutParams();
         lp.rowSpec = GridLayout.spec(0, 2);
+        lp.setGravity(Gravity.CENTER);
         lp.leftMargin = -2;
-        products.addView(x, lp);
+        addView(x, lp);
 
         TextView title = new TextView(getContext());
         title.setTextSize(getContext().getResources().getDimension(R.dimen.font_size_medium));
         title.setText(data.getString(6));
         title.setTypeface(null, Typeface.BOLD);
-        title.setPadding(0, 0, 0, - getContext().getResources().getDimensionPixelSize(R.dimen.padding_small));
+        title.setPadding(0, 1, 0, - getContext().getResources().getDimensionPixelSize(R.dimen.padding_small));
+        title.setTextColor(getResources().getColor(R.color.xlight_blue));
         lp = new GridLayout.LayoutParams();
         lp.columnSpec = GridLayout.spec(3, 2);
-//        lp.topMargin = 27;
-        products.addView(title, lp);
+        addView(title, lp);
 
         TextView result = new TextView(getContext());
         result.setText(String.format("%.2f", total));
         result.setTypeface(null, Typeface.BOLD);
-        result.setTextColor(getResources().getColor(android.R.color.black));
-        result.setPadding(0, -padding, 0, 0);
+        result.setTextColor(getResources().getColor(R.color.xdark_green));
         result.setTextSize(getContext().getResources().getDimension(R.dimen.font_size_large));
         lp = new GridLayout.LayoutParams();
         lp.rowSpec = GridLayout.spec(0, 2);
-        lp.setGravity(Gravity.RIGHT);
-        products.addView(result, lp);
+        lp.leftMargin = getContext().getResources().getDimensionPixelSize(R.dimen.padding_xsmall);
+        lp.bottomMargin = - getContext().getResources().getDimensionPixelSize(R.dimen.padding_xsmall);
+        lp.setGravity(Gravity.RIGHT|Gravity.BOTTOM);
+        addView(result, lp);
 
         TextView details = new TextView(getContext());
         details.setTextColor(getResources().getColor(android.R.color.black));
         details.setText(String.format("%.2f", price) + "/St");
         details.setTextSize(getContext().getResources().getDimension(R.dimen.font_size_xsmall));
         lp = new GridLayout.LayoutParams();
+        lp.topMargin = - getContext().getResources().getDimensionPixelSize(R.dimen.padding_small);
         lp.columnSpec = GridLayout.spec(3);
-        products.addView(details, lp);
+        addView(details, lp);
 
         TextView eq = new TextView(getContext());
         eq.setText(" =");
         eq.setTextSize(getContext().getResources().getDimension(R.dimen.font_size_small));
+        eq.setTextColor(getResources().getColor(R.color.light_blue));
         lp = new GridLayout.LayoutParams();
         lp.columnSpec = GridLayout.spec(4);
-        lp.setGravity(Gravity.BOTTOM);
-        lp.bottomMargin = 12;
-        products.addView(eq, lp);
+        lp.topMargin = - getContext().getResources().getDimensionPixelSize(R.dimen.padding_xsmall);
+        lp.setGravity(Gravity.RIGHT);
+        addView(eq, lp);
 
 
     }
