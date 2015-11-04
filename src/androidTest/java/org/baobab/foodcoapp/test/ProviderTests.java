44e812e072178ff3340361cc7f5e4b18bdda88be
orangeman <flo@sonnenstreifen.de>
2015-11-04 10:33:58 +0100

    refactor tests

--- src/androidTest/java/org/baobab/foodcoapp/test/ProviderTests.java
+++ src/androidTest/java/org/baobab/foodcoapp/test/ProviderTests.java
@@ -3,6 +3,7 @@ package org.baobab.foodcoapp.test;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.test.ProviderTestCase2;
 
 import org.baobab.foodcoapp.AccountingProvider;
@@ -20,9 +21,8 @@ public class ProviderTests extends ProviderTestCase2<AccountingProvider> {
 
     public void testAccountBalance() {
         createDummyAccount("dummy");
-        insertTransaction("final", "dummy");
-        Cursor accounts = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts/passiva/accounts"), null, null, null, null);
+        insertTransaction("final", "lager", "dummy");
+        Cursor accounts = query("accounts/passiva/accounts", 4);
         accounts.moveToPosition(3);
         assertEquals("name", "dummy", accounts.getString(1));
         assertEquals("balance", -42.0, accounts.getDouble(4));
@@ -30,31 +30,22 @@ public class ProviderTests extends ProviderTestCase2<AccountingProvider> {
 
     public void testEmptyAccount() {
         createDummyAccount("dummy");
-        Cursor accounts = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts/passiva/accounts"), null, null, null, null);
-        assertEquals("one account", 4, accounts.getCount());
-        Uri transaction = insertTransaction("draft", "dummy");
+        query("accounts/passiva/accounts", 4);
+        Uri transaction = insertTransaction("draft", "lager", "dummy");
         ContentValues b = new ContentValues();
         b.put("account_guid", "dummy");
         b.put("quantity", 42);
         getMockContentResolver().insert(transaction.buildUpon()
                 .appendEncodedPath("products/2").build(), b);
-        accounts = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts/passiva/accounts"), null, null, null, null);
-        assertEquals("one account", 4, accounts.getCount());
+        query("accounts/passiva/accounts", 4);
     }
 
     public void testListAccounts() {
+        query("accounts", 11);
         createDummyAccount("dummy");
-        Cursor accounts = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts"), null, null, null, null);
-        assertEquals("twelve accounts in total", 12, accounts.getCount());
-        accounts = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts/aktiva/accounts"), null, null, null, null);
-        assertEquals("no aktiva account", 6, accounts.getCount());
-        accounts = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts/passiva/accounts"), null, null, null, null);
-        assertEquals("one passiva account", 4, accounts.getCount());
+        query("accounts", 12);
+        query("accounts/aktiva/accounts", 6);
+        query("accounts/passiva/accounts", 4);
     }
 
     public void testFindAccountByName() {
@@ -73,16 +64,24 @@ public class ProviderTests extends ProviderTestCase2<AccountingProvider> {
     public void testKontoauszug() {
         createDummyAccount("dummy");
         createDummyAccount("another");
-        insertTransaction("final", "dummy");
-        insertTransaction("final", "another");
-        Cursor transactions = getMockContentResolver().query(Uri.parse(
-                "content://org.baobab.foodcoapp.test/accounts/dummy/transactions"), null, null, null, null);
-        assertEquals("one transaction", 1, transactions.getCount());
-        transactions.moveToFirst();
+        insertTransaction("final", "lager", "dummy");
+        insertTransaction("final", "lager", "another");
+        Cursor transactions = query("accounts/dummy/transactions", 1);
         assertEquals("time", 1, transactions.getLong(2));
         assertEquals("who", "dummy", transactions.getString(3));
         assertEquals("sum", 42.0, transactions.getDouble(6));
+        assertEquals("passiva", "passiva", transactions.getString(9));
         assertEquals("Bilanzerniedrigung", true, transactions.getInt(8) < 0);
+        assertEquals("involved accounts", "dummy,lager", transactions.getString(5));
+    }
+
+    @NonNull
+    private Cursor query(String path, int assert_count) {
+        Cursor transactions = getMockContentResolver().query(Uri.parse(
+                "content://org.baobab.foodcoapp.test/" + path), null, null, null, null);
+        assertEquals("number of results", assert_count, transactions.getCount());
+        transactions.moveToFirst();
+        return transactions;
     }
 
     private Uri createDummyAccount(String name) {
@@ -94,19 +93,19 @@ public class ProviderTests extends ProviderTestCase2<AccountingProvider> {
                 Uri.parse("content://org.baobab.foodcoapp.test/accounts/passiva/accounts"), values);
     }
 
-    private Uri insertTransaction(String status, String account) {
+    private Uri insertTransaction(String status, String from_account, String to_account) {
         ContentValues t = new ContentValues();
         t.put("status", status);
         t.put("stop", 1);
         Uri transaction = getMockContentResolver().insert(Uri.parse(
                 "content://org.baobab.foodcoapp.test/transactions"), t);
         ContentValues b = new ContentValues();
-        b.put("account_guid", "lager");
+        b.put("account_guid", from_account);
         b.put("quantity", -42);
         getMockContentResolver().insert(transaction.buildUpon()
                 .appendEncodedPath("products/5").build(), b);
         b = new ContentValues();
-        b.put("account_guid", account);
+        b.put("account_guid", to_account);
         b.put("quantity", 42);
         getMockContentResolver().insert(transaction.buildUpon()
                 .appendEncodedPath("products/2").build(), b);
