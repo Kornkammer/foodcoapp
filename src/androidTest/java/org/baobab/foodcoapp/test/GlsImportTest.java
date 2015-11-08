338632ab3db8bb6648e4ef7f2fb186715ea1a0c4
orangeman <flo@sonnenstreifen.de>
2015-11-08 18:36:06 +0100

    gls import

---
+++ src/androidTest/java/org/baobab/foodcoapp/test/GlsImportTest.java
@@ -0,0 +1,319 @@
+package org.baobab.foodcoapp.test;
+
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.test.mock.MockContext;
+
+import org.baobab.foodcoapp.GlsImporter;
+import org.baobab.foodcoapp.Import;
+
+import java.text.NumberFormat;
+import java.util.Date;
+
+
+public class GlsImportTest extends BaseProviderTests {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        createDummyAccount("Susi", "0815");
+        createDummyAccount("Albert", "1234");
+    }
+
+    public void testEinlage() {
+        assertEinlage("EinlageSusi");
+        assertEinlage("Einlage Susi");
+        assertEinlage("Einlage-Susi");
+        assertEinlage("Einlage: Susi");
+        assertEinlage("Einlage - Susi");
+        assertEinlage("einlage Susi");
+        assertEinlage("einlage-Susi");
+        assertEinlage("einlage: Susi");
+        assertEinlage("einlage - Susi");
+        assertEinlage("Einlage 0815");
+        assertEinlage("Einlage-0815");
+        assertEinlage("Einlage: 0815");
+        assertEinlage("Einlage - 0815");
+        assertEinlage("einlage 0815");
+        assertEinlage("einlage-0815");
+        assertEinlage("einlage: 0815");
+        assertEinlage("einlage - 0815");
+        assertEinlage("Einlage-Susi-0815");
+        assertEinlage("Einlage: Susi-0815");
+        assertEinlage("einlage - Susi - 0815");
+        assertEinlage("Einlage-0815-Susi");
+        assertEinlage("Einlage: 0815-Susi");
+        assertEinlage("einlage - - 0815 : Susi");
+        assertEinlage("Einlage Susi-0815 und noch liebe Grüße");
+        assertVerbindlichkeit("Einlage Xaver", "Unbekanntes Mitglied");
+        assertVerbindlichkeit("Einlage 76345", "Unbekanntes Mitglied");
+        assertVerbindlichkeit("Einlage-Susi-0827", "Falsche MitgliedsNr");
+        assertVerbindlichkeit("Einlage Susi 0827", "Falsche MitgliedsNr");
+        assertVerbindlichkeit("Einlage: 0827 - Susi", "Falsche MitgliedsNr");
+        assertVerbindlichkeit("Einlage-Susssi-0815", "Falscher MitgliedsName");
+        assertVerbindlichkeit("Einlage - 0815 Susssli", "Falscher MitgliedsName");
+        assertVerbindlichkeit("Einlage: 0815 : Susssli", "Falscher MitgliedsName");
+        assertVerbindlichkeit("MaWoasEsNed: DaSchaugHer", "Keine automatische Zuordnung");
+    }
+
+    public void testBeitrag() {
+        assertTrägtBei("Mitgliedsbeitrag Susi");
+        assertTrägtBei("Mitgliedsbeitrag:Susi");
+        assertTrägtBei("Mitgliedsbeitrag-Susi");
+        assertTrägtBei("Mitgliedsbeitrag-0815");
+        assertTrägtBei("Mitgliedsbeitrag: Susi");
+        assertTrägtBei("Mitgliedsbeitrag : Susi");
+        assertTrägtBei("Mitgliedsbeitrag : 0815");
+        assertTrägtBei("Mitgliedsbeitrag - Susi");
+        assertTrägtBei("Mitgliedsbeitrag-Susi: 0815");
+        assertTrägtBei("Mitgliedsbeitrag - 0815: Susi");
+        assertTrägtBei("Beitrag Susi");
+        assertTrägtBei("Beitrag:Susi");
+        assertTrägtBei("Beitrag: Susi");
+        assertTrägtBei("Beitrag : Susi");
+        assertTrägtBei("Beitrag - Susi");
+        assertVerbindlichkeit("Guthaben 7345", "Unbekanntes Mitglied");
+    }
+
+    public void testEinzahlung() {
+        assertZahltEin("Einzahlung Susi");
+        assertZahltEin("Einzahlung:Susi");
+        assertZahltEin("Einzahlung:0815");
+        assertZahltEin("Einzahlung-Susi");
+        assertZahltEin("Einzahlung: Susi");
+        assertZahltEin("Einzahlung : Susi");
+        assertZahltEin("Einzahlung - Susi");
+        assertZahltEin("Einzahlung Susi 0815");
+        assertZahltEin("Einzahlung: 0815 - Susi");
+        assertZahltEin("Guthaben Susi");
+        assertZahltEin("Guthaben:Susi");
+        assertZahltEin("Guthaben: Susi");
+        assertZahltEin("Guthaben : Susi");
+        assertZahltEin("Guthaben - Susi");
+        assertVerbindlichkeit("Guthaben 7345", "Unbekanntes Mitglied");
+    }
+
+    public void testForderungBegleichen() {
+        insertTransaction("0815", "forderungen", 20, "Bank Susi"); // offen
+        insertTransaction("0815", "forderungen", 20, "Bank Andere"); // offen
+        insertTransaction("0815", "forderungen", 20, "andere Forderung");
+        assertBegleichtForderung("Einzahlung - Susi", 20);
+        insertTransaction("0815", "forderungen", 50, "Bank Susi"); // offen
+        assertBegleichtForderung("Einzahlung - Susi", 50);
+        insertTransaction("0815", "forderungen", 50, "Bank Susi"); // offen
+        assertBegleichtForderung("Einzahlung - Susi", 50);
+    }
+
+    public void testForderungBegleichenMitRestguthaben() {
+        insertTransaction("0815", "forderungen", 10, "Bank Susi");
+        insertTransaction("0815", "forderungen", 20, "Bank Susi");
+        insertTransaction("0815", "forderungen", 10, "Bank Susi");
+        read(gls().vwz1("Einzahlung - Susi").amount(50)); // überweisen
+        Cursor items = assertTransaction("Einzahlung - Susi", 4);
+        assertTransactionItem("bank", "Bank", "Cash", 50, 1.0f, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bank Susi", -2.0f, 10, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bank Susi", -1.0f, 20, items);
+        items.moveToNext();
+        assertTransactionItem("0815", "Susi", "Credits", -1.0f, 10, items);
+    }
+
+    public void testBarEinzahlung() {
+        insertTransaction("1234", "forderungen", 20, "Bar Albert"); // 1
+        insertTransaction("0815", "forderungen", 30, "Bar Susi");   // 2
+        insertTransaction("1234", "forderungen", 20, "Bar Albert"); // 3
+        insertTransaction("0815", "forderungen", 10, "Bar Susi");   // 4
+        // kasse wird entleert und eingezahlt. Inzwischen gibt es weitere
+        insertTransaction("1234", "forderungen", 50, "Bar Albert"); // 5
+        insertTransaction("0815", "forderungen", 30, "Bar Susi");   // 6
+        read(gls().vwz1("Barkasse").amount(90)); // überweisung kommt an
+        Cursor items = assertTransaction("Barkasse", 5);
+        assertTransactionItem("bank", "Bank", "Cash", 90, 1.0f, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bar Albert", -2.0f, 20, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bar Susi", -1.0f, 10, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bar Susi", -1.0f, 30, items);
+        items.moveToNext();
+        assertTransactionItem("verbindlichkeiten", "Verbindlichkeiten", "Barkasse", -1.0f, 10, items);
+        read(gls().vwz1("Barkasse").amount(100)); // neue überweisung kommt an
+        items = assertTransaction("Barkasse", 4); // begleicht die zwei weiteren
+        assertTransactionItem("bank", "Bank", "Cash", 100, 1.0f, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bar Albert", -1.0f, 50, items);
+        items.moveToNext();
+        assertTransactionItem("forderungen", "Forderungen", "Bar Susi", -1.0f, 30, items);
+        items.moveToNext();
+        assertTransactionItem("verbindlichkeiten", "Verbindlichkeiten", "Barkasse", -1.0f, 20, items);
+    }
+
+    public void testAnschaffung() {
+        assertBooking("Inventar:Tisch", "Inventar", "Tisch", 3.50f, "Rechnung XY");
+        assertBooking("Inventar: Kiste", "Inventar", "Kiste", 5.50f, "Rechnung abc");
+        assertBooking("Inventar - Stift", "Inventar", "Stift", 1.5f, "Rechnung 234");
+        assertBooking("Inventar: tolle Sache", "Inventar", "tolle Sache", 3.50f, "Rechnung XY");
+        assertBooking("Inventar: auch mit-strich", "Inventar", "auch mit-strich", 3, "Rechnung");
+    }
+
+    public void testAusgaben() {
+        assertBooking("Kosten:Miete April", "Kosten", "Miete April", 350, "Dauerauftrag..");
+        assertBooking("Kosten: Strom und Wasser", "Kosten", "Strom und Wasser", 34, "XYZABC12");
+        assertBooking("Kosten - Versand/Transport", "Kosten", "Versand/Transport", 1.5f, "usw..");
+    }
+
+    public void testRechnungBegleichen() {
+        insertTransaction("verbindlichkeiten", "lager", 100, "Lieferung 123"); // offen
+        assertBooking("Lieferung 123", "Verbindlichkeiten", "Lieferung 123", 100, "bezahlt");
+        insertTransaction("verbindlichkeiten", "lager", 42.23f, "Lieferung 123"); // offen
+        assertBooking("Lieferung 123", "Verbindlichkeiten", "Lieferung 123", 42.23f, "bezahlt");
+        insertTransaction("verbindlichkeiten", "lager", 30, "Lieferung 123"); // offen
+        // Verbindlichkeit nicht begleichen, sondern wenn nicht zugeordnet, dann neue Forderung
+        assertBooking("Lieferung 123", "Forderungen", "Lieferung 123", 50, "bezahlt"); // zuviel
+    }
+
+    public void testIdempotency() { // selber Tag mit selbem VWZ geht nicht
+        read(gls().date("01.05.2015").vwz1("Einzahlung - Susi").amount(50));
+        assertGebucht("Susi", 50, "Einzahlung - Susi");
+        read(gls().date("01.05.2015").vwz1("Einzahlung - Susi").amount(50)); // selbe Buchung
+        assertNichtGebucht();
+        read(gls().date("01.05.2015").vwz1("Einzahlung - Susi").amount(20)); // anderer Betrag
+        assertNichtGebucht();
+        read(gls().date("02.05.2015").vwz1("Einzahlung - Susi").amount(50)); // anderer Tag
+        assertGebucht("Susi", 50, "Einzahlung - Susi");
+        read(gls().date("01.05.2015").vwz1("Guthaben : Susi").amount(50)); // anderer VWZ
+        assertGebucht("Susi", 50, "Guthaben : Susi");
+    }
+
+
+
+
+
+    // HELPER
+
+    private void assertGebucht(String account, float amount, String vwz) {
+        assertTransaction("0815", account, "Credits", "bank", amount, "VWZ: " + vwz);
+    }
+
+    private void assertNichtGebucht() {
+        Import.Result result = importer.store(values);
+        query(result.session.buildUpon().appendPath("transactions").build(), 0);
+    }
+
+    private void assertEinlage(String verwendungszeck1) {
+        read(gls().vwz1(verwendungszeck1).amount(105));
+        assertTransaction("einlagen", "Einlagen", "Susi", "bank", 105, "VWZ: " + verwendungszeck1);
+    }
+
+    private void assertTrägtBei(String verwendungszeck1) {
+        read(gls().vwz1(verwendungszeck1).amount(23.42));
+        assertTransaction("beiträge", "Beiträge", "Susi", "bank", 23.42f, "VWZ: " + verwendungszeck1);
+    }
+
+    private void assertZahltEin(String verwendungszeck1) {
+        read(gls().vwz1(verwendungszeck1).amount(42.23));
+        assertTransaction("0815", "Susi", "Credits", "bank", 42.23f, "VWZ: " + verwendungszeck1);
+    }
+
+    private void assertVerbindlichkeit(String vwz, String comment) {
+        read(gls().vwz1(vwz).amount(42.23));
+        assertTransaction("verbindlichkeiten", "Verbindlichkeiten", vwz, "bank", 42.23f, comment);
+    }
+
+    private void assertBegleichtForderung(String vwz, float amount) {
+        read(gls().vwz1(vwz).amount(amount));
+        assertTransaction("forderungen", "Forderungen", "Bank Susi", "bank", amount, "VWZ: " + vwz);
+    }
+
+    private void assertBooking(String vwz2, String name, String title, float amount, String comment) {
+        read(gls().vwz1(comment).vwz2(vwz2).amount(-amount));
+        Cursor items = assertTransaction(comment, 2);
+        if (items.getString(11).equals("bank")) { // account order
+            assertTransactionItem("bank", "Bank", "Cash", -amount, 1.0f, items);
+            items.moveToNext();
+            assertTransactionItem(name.toLowerCase(), name, title, 1.0f, amount, items);
+        } else {
+            assertTransactionItem(name.toLowerCase(), name, title, 1.0f, amount, items);
+            items.moveToNext();
+            assertTransactionItem("bank", "Bank", "Cash", -amount, 1.0f, items);
+        }
+    }
+
+    private void assertTransaction(String from_account, String fromAccountName,  String fromTitle,
+                                   String to_account, float amount, String comment) {
+        Cursor items = assertTransaction(comment, 2);
+        assertTransactionItem(to_account, "Bank", "Cash", amount, 1.0f, items);
+        items.moveToNext();
+        assertTransactionItem(from_account, fromAccountName, fromTitle, -1.0f, amount, items);
+    }
+    private Cursor assertTransaction(String comment, int assertCount) {
+        Import.Result result = importer.store(values);
+        finalizeSession(result.session);
+        Cursor transactions = query(result.session
+                .buildUpon().appendPath("transactions").build(), 1);
+        assertTrue(transactions.getString(4) + "' should contain '" + comment,
+                transactions.getString(4).contains(comment));
+        return query("transactions/" + transactions.getLong(0), assertCount);
+    }
+
+    private void assertTransactionItem(String guid, String name, String title, float quantity, float price, Cursor items) {
+        assertEquals(guid, items.getString(11));
+        assertEquals(name, items.getString(12));
+        assertEquals(title, items.getString(7));
+        assertEquals(quantity, items.getFloat(4));
+        assertEquals(price, items.getFloat(5));
+    }
+
+
+
+    Context ctx = new MockContext() {
+        @Override
+        public ContentResolver getContentResolver() {
+            return getMockContentResolver();
+        }
+    };
+
+    static { GlsImporter.AUTHORITY = "org.baobab.foodcoapp.test"; }
+    private ContentValues[] values = new ContentValues[1];
+    private GlsImporter importer;
+
+    private void read(Gls gls) {
+        importer = new GlsImporter(ctx);
+        values[0] = importer.read(gls.line);
+    }
+
+    long time = System.currentTimeMillis();
+    private Gls gls() {
+        return new Gls().date(GlsImporter.date.format(new Date(time+=87000000)));
+    }
+
+    class Gls {
+        public String[] line = new String[] {"", "01.02.2015", "", "", "",
+                "", "", "", "", "", "", "", "", "", "", "", "", "", "", "20.3"};
+
+        public Gls date(String date) {
+            line[0] = date;
+            line[1] = date;
+            return this;
+        }
+
+        public Gls vwz1(String vwz1) {
+            line[3] = vwz1;
+            return this;
+        }
+
+        public Gls vwz2(String vwz2) {
+            line[4] = vwz2;
+            return this;
+        }
+
+        public Gls amount(double amount) {
+            line[19] = NumberFormat.getInstance().format(amount);
+            return this;
+        }
+    }
+}
---
+++ src/main/java/org/baobab/foodcoapp/GlsImporter.java
@@ -0,0 +1,268 @@
+package org.baobab.foodcoapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GlsImporter implements Import.Importer {
+
+    public static SimpleDateFormat date = new SimpleDateFormat("dd.MM.yyyy");
+    public static String AUTHORITY = "org.baobab.foodcoapp";
+    private final Context ctx;
+    private String msg = "";
+    private int count = 0;
+    public final Uri uri;
+
+    public GlsImporter(Context ctx) {
+        this.ctx = ctx;
+        ContentValues cv = new ContentValues();
+        cv.put("start", System.currentTimeMillis());
+        uri = ctx.getContentResolver().insert(Uri.parse(
+                "content://" + AUTHORITY + "/sessions"), cv);
+    }
+
+    static final Pattern vwz1 = Pattern.compile(
+            "^(Einlage|einlage|Mitgliedsbeitrag|mitgliedsbeitrag|Beitrag|beitrag" +
+                    "|Einzahlung|einzahlung|Guthaben|guthaben|Barkasse|barkasse)" +
+                    "[-:\\s]*([\\da-zA-Z]*)([-:\\s]*|$)([\\da-zA-Z]*)([-:\\s]*|$).*");
+
+    static final Pattern vwz2 = Pattern.compile("^([^-:\\s]*)[-:\\s]+(.*)([-:\\s]*|$)+.*");
+
+    @Override
+    public ContentValues read(String[] line) {
+        try {
+            String comment = "VWZ: " + line[3];
+            long time = date.parse(line[1]).getTime();
+            float amount = NumberFormat.getInstance().parse(line[19]).floatValue();
+            if (amount > 0) {
+                Matcher m = vwz1.matcher(line[3]);
+                Account account = null;
+                if (m.matches()) {
+                    account = findAccount(m.group(2), m.group(4));
+                } else {
+                    comment = "Keine automatische Zuordnung\n" + comment;
+                }
+                if (account != null && account.err != null) {
+                    comment = account.err + "\n" + comment;
+                }
+                Uri transaction = storeTransaction(time, "Bankeingang:\n" + comment);
+                if (transaction == null) {
+                    msg += "\nTransaktion gibts schon! " + comment;
+                    return null;
+                }
+                storeBankCash(transaction, amount);
+                if (account != null && account.guid != null && account.err == null) {
+                    if (m.group(1).equalsIgnoreCase("Einzahlung") ||
+                                m.group(1).equalsIgnoreCase("Guthaben")) {
+                        String title = "Bank " + account.name;
+                        Iterator<Long> iter = findOpenTransactions("forderungen", "title IS '" + title + "'");
+                        while (iter.hasNext()) {
+                            Cursor txn = query("forderungen", "transactions._id =" + iter.next());
+                            txn.moveToFirst();
+                            float sum = txn.getFloat(8) * txn.getFloat(11);
+                            if (amount + sum >= 0) { // quantity negative after groupBy from users perspective
+                                storeTransactionItem(transaction, "forderungen", sum, title);
+                                amount += sum;
+                            }
+                        }
+                        if (amount > 0) { // rest guthaben
+                            storeTransactionItem(transaction, account.guid, - amount, "Credits");
+                        }
+                    } else if (m.group(1).equalsIgnoreCase("Mitgliedsbeitrag") ||
+                                m.group(1).equalsIgnoreCase("Beitrag")) {
+                        storeTransactionItem(transaction, "beiträge", - amount, account.name);
+                    } else if (m.group(1).equalsIgnoreCase("Einlage")) {
+                        storeTransactionItem(transaction, "einlagen", - amount, account.name);
+                    }
+                } else if (m.matches() && m.group(1).equalsIgnoreCase("Barkasse")) {
+                    Iterator<Long> iter = findOpenTransactions("forderungen", "title LIKE 'Bar%'");
+                    while (iter.hasNext()) {
+                        Cursor txn = query("forderungen", "transactions._id =" + iter.next());
+                        txn.moveToNext();
+                        float sum = txn.getFloat(8) * txn.getFloat(11);
+                        if (amount + sum >= 0) { // quantity negative after groupBy from users perspective
+                            storeTransactionItem(transaction, "forderungen",
+                                    sum, "Bar " + txn.getString(3));
+                            amount += sum;
+                        }
+                    }
+                    if (amount > 0) { // rest barkasse (should never happen!)
+                        storeTransactionItem(transaction, "verbindlichkeiten", - amount, "Barkasse");
+                    }
+                } else {
+                    storeTransactionItem(transaction, "verbindlichkeiten", - amount, line[3]);
+                }
+                count++;
+                return new ContentValues();
+            } else { // amount < 0
+                Matcher m = vwz2.matcher(line[4]);
+                Account account = null;
+                if (m.matches()) {
+                    account = findAccount(m.group(1), "");
+                } else {
+                    comment = "VWZ nicht erkannt\n" + comment;
+                }
+                if (account != null && account.err != null) {
+                    comment = account.err + "\n" + comment;
+                }
+                Uri transaction = storeTransaction(time, "Bankausgang:\n" + comment);
+                if (transaction == null) {
+                    msg += "\nTransaktion gibts schon! " + comment;
+                    return null;
+                }
+                storeBankCash(transaction, amount);
+                if (account != null && account.guid != null && account.err == null) {
+                    storeTransactionItem(transaction, account.guid, -amount, m.group(2));
+                    amount = 0;
+                } else {
+                    Iterator<Long> iter = findOpenTransactions("verbindlichkeiten", "title IS '" + line[4] + "'");
+                    if (iter.hasNext()) {
+                        Cursor txn = query("verbindlichkeiten", "transactions._id =" + iter.next());
+                        txn.moveToFirst();
+                        float sum = txn.getFloat(8) * txn.getFloat(11);
+                        if (sum == amount) {
+                            storeTransactionItem(transaction, "verbindlichkeiten", -amount, line[4]);
+                            amount = 0;
+                        }
+                    }
+                }
+                if (amount < 0) { // still
+                    storeTransactionItem(transaction, "forderungen", -amount, line[4]);
+                }
+            }
+            count++;
+            return new ContentValues();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            msg += "\nError! " + e.getMessage();
+            return null;
+        }
+    }
+
+    private Iterator<Long> findOpenTransactions(String guid, String selection) {
+        Cursor products = ctx.getContentResolver().query(Uri.parse(
+                        "content://" + AUTHORITY + "/accounts/" + guid + "/products"),
+                            null, selection, null , null);
+        TreeSet<Long> ids = new TreeSet<>();
+        while (products.moveToNext()) {
+            Cursor txns = query(guid, "title IS '" + products.getString(7) + "'" +
+                " AND price = " + products.getFloat(5) + // select before groupBy
+                (products.getFloat(4) > 0? " AND quantity > 0" : " AND quantity < 0"));
+            txns.moveToLast();
+            for (int i = 0; i < Math.abs(products.getFloat(4)); i++) {
+                ids.add(txns.getLong(0));
+                if (!txns.isFirst()) {
+                    txns.moveToPrevious();
+                }
+            }
+        }
+        return ids.iterator();
+    }
+
+    private Cursor query(String guid, String selection) {
+        return ctx.getContentResolver().query(Uri.parse(
+                        "content://" + AUTHORITY + "/accounts/" + guid + "/transactions"),
+                null, selection + " AND transactions.status IS NOT 'draft'", null , null);
+    }
+
+    private void storeBankCash(Uri transaction, float amount) {
+        ContentValues b = new ContentValues();
+        b.put("account_guid", "bank");
+        b.put("product_id", 1);
+        b.put("title", "Cash");
+        b.put("quantity", amount);
+        b.put("price", 1.0);
+        ctx.getContentResolver().insert(transaction.buildUpon()
+                .appendEncodedPath("products").build(), b);
+    }
+
+    private Uri storeTransaction(long time, String comment) {
+        ContentValues t = new ContentValues();
+        t.put("session_id", uri.getLastPathSegment());
+        t.put("stop", time);
+        t.put("status", "draft");
+        t.put("comment", comment);
+        return ctx.getContentResolver().insert(Uri.parse(
+                "content://" + AUTHORITY + "/transactions"), t);
+    }
+
+    private void storeTransactionItem(Uri transaction, String account, float amount, String title) {
+        ContentValues b = new ContentValues();
+        b.put("account_guid", account);
+        b.put("product_id", 2);
+        b.put("title", title);
+        b.put("quantity", amount > 0? 1: -1);
+        b.put("price", Math.abs(amount));
+        ctx.getContentResolver().insert(transaction.buildUpon()
+                .appendEncodedPath("products").build(), b);
+    }
+
+    static class Account {
+        String guid;
+        String name;
+        String err;
+    }
+    private Account findAccount(String name, String guid) {
+        Account a = findAccountBy("name", name);
+        if (a.guid == null) {
+            a = findAccountBy("guid", name);
+            if (a.guid == null) {
+                a.err = "Unbekanntes Mitglied";
+            }
+            if (!guid.equals("")) {
+                Account b = findAccountBy("name", guid);
+                if (b.guid != null) {
+                    if (!b.guid.equals(a.guid)) {
+                        a.err = "Falsche MitgliedsNr";
+                    }
+                } else {
+                    b = findAccountBy("guid", guid);
+                    if (b.guid != null) {
+                        if (!b.guid.equals(a.guid)) {
+                            a.err = "Falscher MitgliedsName";
+                        }
+                    } else { // there is second param that is not found
+                        a.err = "Falscher MitgliedsName";
+                    }
+                }
+            }
+            return a;
+        }
+        if (a.guid != null && !guid.equals("")) {
+            Account b = findAccountBy("guid", guid);
+            if (!a.guid.equals(b.guid)) {
+                a.err = "Falsche MitgliedsNr";
+            }
+        }
+        return a;
+    }
+    private Account findAccountBy(String column, String value) {
+        Account a = new Account();
+        Cursor accounts = ctx.getContentResolver().query(Uri.parse(
+                        "content://" + AUTHORITY + "/accounts"), null,
+                        column + " IS ?", new String[] { value }, null);
+        if (accounts.getCount() == 1) {
+            accounts.moveToFirst();
+            a.name = accounts.getString(1);
+            a.guid = accounts.getString(2);
+        }
+        return a;
+    }
+
+    @Override
+    public Import.Result store(ContentValues[] values) {
+        Import.Result result = new Import.Result();
+        result.session = uri;
+        result.msg = "Imported " + count + " transactions (von " + values.length + msg;
+        return result;
+    }
+}
