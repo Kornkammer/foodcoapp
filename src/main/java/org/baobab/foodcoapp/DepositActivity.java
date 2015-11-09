262ed4804ce7f14a74413b22af2a7510ebbc24a0
orangeman <flo@sonnenstreifen.de>
2015-11-09 18:26:09 +0100

    deposit explaining alert

--- src/main/java/org/baobab/foodcoapp/DepositActivity.java
+++ src/main/java/org/baobab/foodcoapp/DepositActivity.java
@@ -1,13 +1,15 @@
 package org.baobab.foodcoapp;
 
 import android.annotation.TargetApi;
-import android.app.ProgressDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -39,7 +41,7 @@ public class DepositActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            float amount;
+            final float amount;
             try {
                 amount = NumberFormat.getInstance().parse(
                         ((EditText) findViewById(R.id.amount)).getText().toString()).floatValue();
@@ -47,40 +49,65 @@ public class DepositActivity extends AppCompatActivity {
                 Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
                 return;
             }
-            String title = null;
+            final String title;
+            final String guid = data.getStringExtra("guid");
+            final String name = data.getStringExtra("name");
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
             if (((RadioButton) findViewById(R.id.bar)).isChecked()) {
                 title = "Bar " + data.getStringExtra("name");
+                alert.setMessage(String.format(getString(R.string.msg_deposit_cash),
+                        String.format("%.2f", amount), guid, name));
             } else {
                 title = "Bank " + data.getStringExtra("name");
+                alert.setMessage(String.format(getString(R.string.msg_deposit_bank),
+                        String.format("%.2f", amount), guid, name));
             }
-            ContentValues t = new ContentValues();
-            t.put("status", "final");
-            t.put("comment", title + "\n" +
-                    ((EditText) findViewById(R.id.comment)).getText().toString());
-            Uri transaction = getContentResolver().insert(Uri.parse(
-                    "content://org.baobab.foodcoapp/transactions"), t);
-            ContentValues b = new ContentValues();
-            b.put("product_id", 1);
-            b.put("account_guid", "forderungen");
-            b.put("title", title);
-            b.put("quantity", 1);
-            b.put("price", amount);
-            getContentResolver().insert(transaction.buildUpon()
-                    .appendEncodedPath("products").build(), b);
-            b = new ContentValues();
-            b.put("product_id", 2);
-            b.put("title", "Credits");
-            b.put("account_guid", data.getStringExtra("guid"));
-            b.put("quantity", - 1);
-            b.put("price", amount);
-            getContentResolver().insert(transaction.buildUpon()
-                    .appendEncodedPath("products").build(), b);
-            Toast.makeText(this, "Guthaben aufgeladen \n" + amount, Toast.LENGTH_LONG).show();
-            finish();
-            startActivity(new Intent(this, TransactionsActivity.class)
-                    .setData(Uri.parse("content://org.baobab.foodcoapp/accounts/" +
-                            data.getStringExtra("guid") + "/transactions")));
+            alert.setNegativeButton(R.string.btn_no, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(2000);
+                }
+            })
+            .setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    store(guid, amount, title);
+                }
+            }).show();
+
         }
+    }
+
+    private void store(String guid, float amount, String title) {
+        Toast.makeText(this, "Guthaben aufgeladen \n" + amount, Toast.LENGTH_LONG).show();
+        ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(200);
+        MediaPlayer.create(this, R.raw.chaching).start();
+        ContentValues t = new ContentValues();
+        t.put("status", "final");
+        t.put("comment", title + "\n" +
+                ((EditText) findViewById(R.id.comment)).getText().toString());
+        Uri transaction = getContentResolver().insert(Uri.parse(
+                "content://org.baobab.foodcoapp/transactions"), t);
+        ContentValues b = new ContentValues();
+        b.put("product_id", 1);
+        b.put("account_guid", "forderungen");
+        b.put("title", title);
+        b.put("quantity", 1);
+        b.put("price", amount);
+        getContentResolver().insert(transaction.buildUpon()
+                .appendEncodedPath("products").build(), b);
+        b = new ContentValues();
+        b.put("product_id", 2);
+        b.put("title", "Credits");
+        b.put("account_guid", guid);
+        b.put("quantity", - 1);
+        b.put("price", amount);
+        getContentResolver().insert(transaction.buildUpon()
+                .appendEncodedPath("products").build(), b);
+        finish();
+        startActivity(new Intent(this, TransactionsActivity.class)
+                .setData(Uri.parse("content://org.baobab.foodcoapp/accounts/" +
+                        guid + "/transactions")));
     }
 
     @Override
