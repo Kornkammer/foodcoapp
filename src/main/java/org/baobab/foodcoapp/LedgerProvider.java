2ea63d09a85b10d48de63968e67fd50992d71d9d
orangeman <flo@sonnenstreifen.de>
2016-05-01 17:20:13 +0200

    fix unit liter and kilo

--- src/main/java/org/baobab/foodcoapp/LedgerProvider.java
+++ src/main/java/org/baobab/foodcoapp/LedgerProvider.java
@@ -256,7 +256,7 @@ public class LedgerProvider extends ContentProvider {
                 }
                 result = db.getReadableDatabase().rawQuery(
                         "SELECT transaction_products._id, transaction_id, account_guid," +
-                                " product_id, sum(quantity) AS stock, ROUND(price, 2) AS price, unit, title, img," +
+                                " product_id, sum(quantity) AS stock, ROUND(price, 2) AS price, MAX(unit), title, img," +
                                 " accounts._id, parent_guid, guid, name" +
                         " FROM transaction_products" +
                         " LEFT JOIN (" +
