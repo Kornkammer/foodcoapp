7da4e21b6bcc66a0100c9a21d26e6ae0ed8bc9f7
orangeman <flo@sonnenstreifen.de>
2016-05-05 18:52:35 +0200

    export mitglieder liste

--- src/main/java/org/baobab/foodcoapp/io/BackupExport.java
+++ src/main/java/org/baobab/foodcoapp/io/BackupExport.java
@@ -15,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.zip.ZipEntry;
@@ -24,7 +25,7 @@ import au.com.bytecode.opencsv.CSVWriter;
 
 public class BackupExport {
 
-    static SimpleDateFormat df = new SimpleDateFormat();
+    public static SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 
     public static File create(Context ctx, String id) {
 
@@ -41,7 +42,7 @@ public class BackupExport {
             zip(null, new File(Environment.getDataDirectory(),
                     "//data//org.baobab.foodcoapp//databases//" + db),
                     "Knk_" + date + ".BAK", zos);
-            transactions(ctx, zos);
+            transactions(ctx, zos, 0);
             reports(ctx, zos);
             lager(ctx, zos);
 
@@ -54,33 +55,135 @@ public class BackupExport {
         return result;
     }
 
-    static void transactions(final Context ctx, ZipOutputStream zos) throws IOException {
+    static void transactions(final Context ctx, ZipOutputStream zos, int year) throws IOException {
+        File tmp = null;
+        SimpleDateFormat d = new SimpleDateFormat("yyyy");
+        String selection = "transactions.status IS NOT 'draft'";
+        if (year > 0) {
+            try {
+                long from = d.parse("" + year).getTime();
+                long to = d.parse("" + (year + 1)).getTime();
+                selection += " AND " + from + " <= transactions.start AND transactions.start < " + to;
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
         Cursor all = ctx.getContentResolver().query(
                 Uri.parse("content://org.baobab.foodcoapp/transactions"),
-                null, null, null, null);
-        File out = file("transactions.csv");
-        exportTransactions(ctx, all, out);
-        zip(null, out, zos);
-        out.delete();
+                null, selection, null, "transactions.start");
+        if (year > 0) {
+            tmp = file(year + "_transactions.csv");
+            exportTransactions(ctx, all, tmp);
+            zip("" + year, tmp, zos);
+        } else {
+            all.moveToFirst();
+            int first = Integer.parseInt(d.format(all.getLong(2)));
+            all.moveToLast();
+            int last = Integer.parseInt(d.format(all.getLong(2)));
+            System.out.println("first " + first + "  last " + last);
+            for (int y = first; y <= last; y++) {
+                transactions(ctx, zos, y);
+            }
+            all.moveToPosition(-1);
+            tmp = file("all_transactions.csv");
+            exportTransactions(ctx, all, tmp);
+            zip(null, tmp, zos);
+        }
+        tmp.delete();
         all.close();
     }
 
     static void reports(final Context ctx, ZipOutputStream zos) throws IOException {
-        Cursor accounts = ctx.getContentResolver().query(
-                Uri.parse("content://org.baobab.foodcoapp/accounts/mitglieder/accounts"),
-                null, null, null, null);
-        for (int i = 0; i < accounts.getCount(); i++) {
-            accounts.moveToPosition(i);
-            Cursor account = ctx.getContentResolver().query(
-                    Uri.parse("content://org.baobab.foodcoapp/accounts/" +
-                            accounts.getString(2) + "/transactions"),
-                    null, null, null, null);
-            File csv = file(accounts.getString(1) + "_" + accounts.getString(2) + ".csv");
-            exportTransactions(ctx, account, csv);
-            zip("Kontoumsätze", csv, zos);
-            csv.delete();
-            account.close();
+        Cursor accounts = getMembers(ctx, "", null);
+        File tmp = file("mitglieder.csv");
+        CSVWriter csv;
+        try {
+            csv = new CSVWriter(new FileWriter(tmp), ';', CSVWriter.NO_QUOTE_CHARACTER);
+            csv.writeNext(new String[] {
+                    "Nr", "Name", "Beitritt",
+                    "Einlage", "",
+                    "Beitrag", "",
+                    "soll", "ist", "",
+                    "Korns", "ein", "aus", "gut",
+                    "Einlage zurück", "" });
+            csv.writeNext(new String[] { } );
+            while (accounts.moveToNext()) {
+                String guid = accounts.getString(2);
+                String name = accounts.getString(1);
+                report(ctx, zos, guid, name);
+                Cons einlage = getContribution(ctx, "?credit=true", name, "einlagen");
+                Cons auslage = getContribution(ctx, "?debit=true", name, "einlagen");
+                Cons beitrag = getContribution(ctx, "", name, "beiträge");
+                long joined = accounts.getLong(5);
+                if (joined == 0) joined = 1431900000000l;
+                int fee = accounts.getInt(8);
+                if (fee == 0) fee = 9;
+                int days = (int) ((System.currentTimeMillis() - joined) / 86400000);
+                int soll = Math.round(days * fee * 12f/365f);
+                String balance;
+                if (beitrag.sum > soll) {
+                    balance = "noch " + Math.round((beitrag.sum - soll) / (fee * 12f/365f)) + " Tage gut";
+                } else {
+                    balance = "noch " + Math.round(soll - beitrag.sum) + "€ offen!";
+                }
+                Cursor debit = getMembers(ctx, "?debit=true", name);
+                debit.moveToFirst();
+                Cursor credit = getMembers(ctx, "?credit=true", name);
+                credit.moveToFirst();
+
+                csv.writeNext(new String[] { guid, name, df.format(joined),
+                        "" + einlage.sum, einlage.dates,
+                        "" + fee, "€/Mon an " + days + " Tagen",
+                        "" + soll, "" + Math.round(beitrag.sum), balance, "",
+                            String.format("%.2f", (-1 * credit.getFloat(3))),
+                            String.format("%.2f", (-1 * debit.getFloat(3))),
+                            String.format("%.2f", (-1 * accounts.getFloat(3))),
+                        "" + auslage.sum, auslage.dates } );
+            }
+            csv.close();
+            zip(null, tmp, zos);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static Cursor getMembers(Context ctx, String query, String name) {
+        return ctx.getContentResolver().query(
+                    Uri.parse("content://org.baobab.foodcoapp/accounts/mitglieder/accounts" + query),
+                    null, (name != null? "name is '" + name + "'" : null), null, null);
+    }
+
+    private static class Cons {
+        String dates = "";
+        float sum;
+    }
+    private static Cons getContribution(Context ctx, String query, String title, String account) {
+        Cons cons = new Cons();
+        Cursor txns = getTxns(ctx, query, title, account);
+        if (txns.getCount() > 0) cons.dates = "€ am ";
+        while (txns.moveToNext()) {
+            cons.sum += (-1 * txns.getFloat(6));
+            cons.dates += df.format(txns.getLong(2)) + ", ";
+        }
+        return cons;
+    }
+    private static Cursor getTxns(Context ctx, String query, String title, String account) {
+        Cursor txns = ctx.getContentResolver().query(
+                Uri.parse("content://org.baobab.foodcoapp/accounts/" + account + "/transactions" + query),
+                null, "transactions.status IS NOT 'draft' AND title IS '" + title + "'", null, null);
+        return txns;
+    }
+
+    private static void report(Context ctx, ZipOutputStream zos, String guid, String name) throws IOException {
+        Cursor account = ctx.getContentResolver().query(
+                Uri.parse("content://org.baobab.foodcoapp/accounts/" +
+                        guid + "/transactions"),
+                null, null, null, null);
+        File csv = file(guid + "_" + name + "_transactions.csv");
+        exportTransactions(ctx, account, csv);
+        zip("Mitglieder", csv, zos);
+        csv.delete();
+        account.close();
     }
 
     static void lager(final Context ctx, ZipOutputStream zos) throws IOException {
@@ -94,7 +197,7 @@ public class BackupExport {
     private static void exportTransactions(final Context ctx, final Cursor c, final File file) {
         CSVWriter out;
         try {
-            out = new CSVWriter(new FileWriter(file));
+            out = new CSVWriter(new FileWriter(file), ';', CSVWriter.NO_QUOTE_CHARACTER);
             while (c.moveToNext()) {
                 out.writeNext(writeTransaction(c));
             }
